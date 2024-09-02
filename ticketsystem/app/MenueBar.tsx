6422866b1d8eccc2ffd7d6415bec932f0c39ce9e
HFGF <HFGF@DESKTOP-3V2KPST>
2024-09-02 09:43:58 +0200

    can now open tickets

--- ticketsystem/app/MenueBar.tsx
+++ ticketsystem/app/MenueBar.tsx
@@ -5,9 +5,9 @@ import styles from './MenueBar.module.css';
 const MenueBar = () => {
   return (
     <nav className={styles.navMenu}>
-      <Link href="/OpenTickets" className={styles.link}>OpenTickets</Link>
-      <Link href="/ClosedTickets" className={styles.link}>ClosedTickets</Link>
-      <Link href="/CreateTicket" className={styles.link}>CreateTicket</Link>
+      <Link href="/OpenTickets" className={styles.link}>Open Tickets</Link>
+      <Link href="/ClosedTickets" className={styles.link}>Closed Tickets</Link>
+      <Link href="/CreateTicket" className={styles.link}>Create Ticket</Link>
       <Link href="/FAQ" className={styles.link}>FAQ</Link>
       <Link href="/SLA" className={styles.link}>SLA</Link>
       <div className={styles.dot}></div>
